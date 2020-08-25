ba9dd334651f38be3fbee53b03c94aeb146c25ce
Basem98 <basem.mostafa98@hotmail.com>
2020-08-25 21:30:49 +0200

    refactor: Implement matrices instead of using individual properties

--- gameObjects.h
+++ gameObjects.h
@@ -7,26 +7,6 @@ typedef struct MainWindow
     const int height;
 } MainWindow;
 
-/**
- * The coordinates for each line
- * that will be used to draw the Spaceship
- */
-typedef struct SpaceLine
-{
-    float x1;
-    float y1;
-    float x2;
-    float y2;
-} SpaceLine;
-
-/**
- * The Spaceship's head
- */
-typedef struct SpaceHead
-{
-    float x;
-    float y;
-} SpaceHead;
 
 /**
  * The Spaceship
@@ -34,9 +14,9 @@ typedef struct SpaceHead
 typedef struct Spaceship
 {
     float vx, vy;
-    SpaceLine *spaceLine[4];
-    SpaceHead *head;
+    float body[2][8];
+    float head[2][1];
+    float centerOfRotation[2][1];
     float direction;
-    SpaceHead *centerOfRotation;
     ALLEGRO_COLOR color;
 } Spaceship;
