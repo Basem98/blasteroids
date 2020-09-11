62ab4f5d36eb1b0b382218f92f774d06d52a4c8f
Basem98 <basem.mostafa98@hotmail.com>
2020-09-11 18:27:58 +0200

    refactor: Include a header file for macros

---
+++ myMacros.h
@@ -0,0 +1,7 @@
+/**
+ * Necessary macros used in multiple object files throughout the program
+ */
+
+
+/* A macro that gets the number of columns in a matrix */
+#define NUM_OF_COLUMNS(x) sizeof(*(x)) / sizeof(float)
